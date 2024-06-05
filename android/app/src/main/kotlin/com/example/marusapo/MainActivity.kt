6c6f5972abaf7950e3901cb479334155a7c3b26b
toratan55.browny <toratan55.browny@gmail.com>
2024-06-05 10:55:30 +0000

    firebaseをflutterで使うためのセットアップ
    package名の変更: com.example.myapp -> com.example.marusapo

---
+++ android/app/src/main/kotlin/com/example/marusapo/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.marusapo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 import 'package:flutter/material.dart';
 import 'package:myapp/widgets/plan_screen.dart';
 import 'package:myapp/util/json_loader.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(MyApp());
 }
 
